d061356778d196be1bb24127d84fec586135163a
Tim Galloway <galloway@iinet.net.au>
2018-06-11 20:40:55 +0800

    Removed unused columns from select statement

--- geojson.php
+++ geojson.php
@@ -9,7 +9,7 @@
 # Connect to PostgreSQL database
 $conn = new PDO('pgsql:host=localhost;dbname=biologically_important_areas','tim','SuzTL1000');
 # Build SQL SELECT statement and return the geometry as a GeoJSON element
-$sql = 'SELECT gid,species_gr,region,taxon_id,genus,species,com_name,legend,location,breeding,foraging,migration,aggregate,other,occurrence,use_level,level_desc,eco_reason,season,comments,extra_com,expert_nam,source_cat,source_des,contact,reviewed,review_dat,review_com,program,spat_date,threatened,migratory_,marine_epb,cetacean_e,sci_advice,shape_area,shape_len, public.ST_AsGeoJSON(public.ST_Transform((geom),4326),6) AS geojson FROM biologically_important_areas WHERE ST_DWithin(geom, ST_MakePoint(113.53538900000001, -25.928723)::geography, 10000);  ';
+$sql = 'SELECT gid,species_gr,region,taxon_id,genus,species,com_name,legend,location,public.ST_AsGeoJSON(public.ST_Transform((geom),4326),6) AS geojson FROM biologically_important_areas WHERE ST_DWithin(geom, ST_MakePoint(113.53538900000001, -25.928723)::geography, 10000);  ';
 /*
 * If bbox variable is set, only return records that are within the bounding box
 * bbox should be a string in the form of 'southwest_lng,southwest_lat,northeast_lng,northeast_lat'
